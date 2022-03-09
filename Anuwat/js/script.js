d67e1cfc6a373776d72859efe4ae497d6df471b5
Anuwat Chaichana (Art) <101080806+Anuwat-ODT@users.noreply.github.com>
2022-03-09 18:52:54 +0700

    NO00 Update (20220309)

---
+++ Anuwat/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.lineup__slider').slick({
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        infinite: false,
+    });
+
+    $('a[href^=#]').on('click',function () {
+        var speed = 500;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position - 30}, speed, "swing");
+        return false;
+      });
+});
